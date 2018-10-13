175cf45f67715f8e8e6bfaf82e04e5aa3a97cc30
Davin Seju <davsej100@gmail.com>
2018-10-12 18:45:59 -0700

    The code

---
+++ ProjectileMotion/src/JavaApplication12.java
@@ -0,0 +1,30 @@
+
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+import java.awt.Color;
+import java.awt.Graphics;
+import javax.swing.JPanel;
+
+/**
+ *
+ * @author sejudav20
+ */
+public class JavaApplication12 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      NewJFrame jf=new NewJFrame();
+      
+      jf.setVisible(true);
+  
+        jf.run();
+    
+    }}
